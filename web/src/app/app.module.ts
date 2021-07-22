a2bfc6bf8971aca994fe63274000c6eb2677aeef
mledium <mledium@mail.ru>
2021-07-23 02:26:16 +0300

    Created web

---
+++ web/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
